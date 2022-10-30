583b1450169658ce0a81dd7f52113e0f625329c1
Rennan Santos <rennansantos@outlook.com>
2022-10-30 11:32:44 -0300

    temas_cores_main

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,30 @@ class ExpansesApp extends StatefulWidget {
 class _ExpansesAppState extends State<ExpansesApp> {
   @override
   Widget build(BuildContext context){
+     final ThemeData tema = ThemeData();
     return   MaterialApp(
+     theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          
+          primary: Colors.purple,
+          secondary: Colors.amber,
+        ),
+        textTheme: tema.textTheme.copyWith(
+          headline6:const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       home:MyHomePage()
     );
   }
