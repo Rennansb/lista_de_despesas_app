8fc23db890d7d903634cb06f56fef39ecb51ea01
Rennan Santos <rennansantos@outlook.com>
2022-10-30 11:13:01 -0300

    stateful_form

--- lib/componets/transaction_form.dart
+++ lib/componets/transaction_form.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
 
-class TransactionForm extends StatelessWidget {
+class TransactionForm extends StatefulWidget {
   
-
-  final titleController = TextEditingController();
-final valueController = TextEditingController();
 
 final void Function(String?, double?) onSubmit;
 
    TransactionForm(this.onSubmit, {super.key});
+
+  @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+
+final valueController = TextEditingController();
 
   _submitForm(){
 final title = titleController.text;
@@ -18,7 +24,7 @@ final title = titleController.text;
                       return;
                     }        
                             
-                            onSubmit(title,value);
+                            widget.onSubmit(title,value);
   }
 
   @override
